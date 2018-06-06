1127b9456cefd2bc4975143648af94a7e11e1a3e
Carl Seiler <cseilerxyzzy@outlook.com>
2018-06-06 14:53:52 -0500

    Clean up ignored files

--- routes/posts.js
+++ routes/posts.js
@@ -1,20 +1,60 @@
 // posts.js
-// 2018-06-04
+// 2018-06-06
 // Description:
 const myLogger = require('winston');
 
 module.exports = {
-  getPosts(req, res) {
-	myLogger.info ('getPosts route requested');
-	},
-  addPost(req, res) {
-	myLogger.info ('addPosts route requested');
-  },
-  updatePost(req, res) {
-	  myLogger.info ('updatePosts route requested');
-
-  },
-  removePost(req, res) {
-	myLogger.info ('removePost route requested');
-  }
+    getPosts(req, res, postStore) {
+        myLogger.info('Posts retrieved.');
+        myLogger.info(JSON.stringify(postStore));
+        res.status(200).send(postStore);
+    },
+    addPost(req, res, postStore) {
+        let newPost = req.body;
+        postStore.posts.push(newPost);
+        let postId = postStore.posts.length - 1;
+        myLogger.info('Post added.');
+        res.status(201).send({ id: postId });
+    },
+    updatePost(req, res, postStore) {
+		let postId = req.params.id;
+        myLogger.info('Preparing to update.');
+        try {
+            if (postId > (postStore.posts.length - 1) || postId < 0) {
+                throw "Id is out of range";
+            }
+        } catch (err) {
+			myLogger.warn ('404: Post not updated. Id is out of range');
+            return res.status(404).send({
+                error: err
+            });
+            // this should end it
+        }
+        postStore.posts[postId] = req.body;
+        myLogger.info('Post updated.');
+        res.status(200).send({
+            id: postId
+        });
+    },
+    removePost(req, res, postStore) {
+		let postId = req.params.id;
+		myLogger.info('Preparing to delete.');
+        try {
+            if (postId > (postStore.posts.length - 1) || postId < 0) {
+                throw "Id is out of range";
+            }
+        } catch (err) {
+			myLogger.warn ('404: Post not deleted. Id is out of range');
+            return res.status(404).send({
+                error: err
+            });
+            // this should end it
+        }
+        postStore.posts.splice(postId, 1); 
+        myLogger.info('Post deleted.');
+        res.status(200).send({
+            id: postId
+        });
+        myLogger.info('removePost route requested');
+    }
 };
--- server.js
+++ server.js
@@ -19,29 +19,17 @@ app.use(errorHander());
 
 let store = {};
 store.posts = [];
-// TODO: Not sure about this:
-store.posts.comments = [];
-let newPost = { name: "Top 10 ES6 Features Every Web Developer Must Know",
-	url: "https://www.example.com/es6",
-	text: "This essay blah blah blah",
-	comments: [] };
-let newComment = { text: "This is the comment" };
-
-// Push a new post object on the array of post objects
-store.posts.push(newPost);
-// Push a new comment string on array of post comments
-store.posts[(store.posts.length - 1)].comments.push(newComment);
-
-let secondComment = { text: "This is the second comment"};
-store.posts[(store.posts.length - 1)].comments.push(secondComment);
-
-myLogger.info (JSON.stringify(store));
 
 app.get('/posts', (req, res) => {
-	posts.getPosts(req, res); 
-	res.status(200).send();
-	});
+    posts.getPosts(req, res, store);
+    });
+app.post('/posts', (req, res) => {
+    posts.addPost(req, res, store);
+    });
+
+app.put('/posts/:id', (req, res) => {
+    posts.updatePost(req, res, store);
+});
 
 app.listen(portNumber);
-myLogger.info(`Started listening on port ${portNumber}`); 
-
+myLogger.info(`Started listening on port ${portNumber}`);
